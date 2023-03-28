bca06ac6fcc88ddf96596764385653ce2ac69221
Valera <leo.plv@bk.ru>
2023-03-28 20:21:46 +0500

    dz2.2

--- JAVAdz/homework2.java
+++ JAVAdz/homework2.java
@@ -1,5 +1,10 @@
 public class homework2 {
-    
+    public static void main(String[] args) {
+        System.out.println(buildString(6, 'a', 'b')); // ababab
+
+        System.out.println(strRLE("aaaabbbcdd"));
+    }
+
     private static String buildString(int n, char first, char second) {
         String str = new String();
         for (int i = 0; i < n; i++) {
@@ -7,21 +12,23 @@ public class homework2 {
         }
         return str;
     }
-    
-   
+
     private static String strRLE(String str) {
-        if (str == "") return str;
+        if (str == "")
+            return str;
         String rle = new String();
         rle += str.charAt(0);
         Integer count = 1;
         for (int i = 1; i < str.length(); i++) {
-            if (str.charAt(i - 1) == str.charAt(i)) count++;
+            if (str.charAt(i - 1) == str.charAt(i))
+                count++;
             else {
                 rle += (count == 1) ? str.charAt(i) : Integer.toString(count) + str.charAt(i);
                 count = 1;
             }
         }
-        if (count > 1) rle += count;
+        if (count > 1)
+            rle += count;
         return rle;
     }
 }
